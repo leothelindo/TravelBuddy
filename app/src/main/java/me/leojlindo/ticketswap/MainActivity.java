52be1f95dbfe3bbd28cc8c62e735564f2fff5f49
Leonardo Lindo <leojlindo@fb.com>
2018-07-17 10:08:08 -0700

    in process of action bar (Set main activity to current start up for now)

---
+++ app/src/main/java/me/leojlindo/ticketswap/MainActivity.java
@@ -0,0 +1,18 @@
+package me.leojlindo.ticketswap;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
